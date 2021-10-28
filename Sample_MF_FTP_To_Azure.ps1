39ab0add1057bc57333d4aa56ff066e03e1eb627
Jeffrey Kraemer <jeffreykraemer@factorization.net>
2021-10-27 21:12:08 -0700

    Updated sample script

--- Sample_MF_FTP_To_Azure.ps1
+++ Sample_MF_FTP_To_Azure.ps1
@@ -10,13 +10,13 @@ $KeepFiles = $true
 $FtpServer = "ftp1-ip.westus2.cloudapp.azure.com"
 $FtpFolder = "'PATH.TO.FOLDER.'"
 $FtpFile = 'test file.txt'  # "PREFIX" + (Get-Date -Format "MMddyy") + "SUFFIX"
-$FtpCredentialPath = '.\ftpcred.xml'
+$FtpCredentialPath = 'ftpcred.xml'
 
 # Azure Settings
 $AzureStorageAccountName = "jeffazureftp"
 $AzureContainerName = "to-auto-hr"
 $AzureFileName = $FtpFile
-$AzureBlobKeyPath = ".\azblobkey.xml"
+$AzureBlobKeyPath = "azblobkey.xml"
 
 # Email Settings
 $CustomerEmail = "jkraemer@ens-inc.com"
@@ -24,7 +24,7 @@ $AdminEmail = "jkraemer@ens-inc.com", "jkraemer@ens-inc.com"
 $FromEmail = "file_transfer@ens-inc.com"
 $SmtpServer = "10.10.10.15"
 $SendSuccessEmail = $true
-$SmtpAuthCredentialPath = ".\smtpcred.xml"
+$SmtpAuthCredentialPath = "smtpcred.xml"
 
 Push-Location
 Set-Location $PSScriptRoot
